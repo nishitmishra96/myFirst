71206d99460a194bc18f5b93c336352a4045670b
nishitmishra <nshtmishra13@gmail.com>
2019-01-28 16:27:13 +0530

    bank file added

---
+++ Bank.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Bank 
+{
+	protected String name;
+	private HeadOffice headOffice;
+	private ArrayList<Branch> branches = new ArrayList<Branch>();
+	
+	
+	protected Bank(String name)
+	{
+		this.name = name;
+		
+	}
+	
+	void createHeadOffice()
+	{
+		this.headOffice = new HeadOffice();
+	}
+	
+	public ArrayList<Branch> getBranches() {
+		return branches;
+	}
+	public void setBranches(Branch branch) {
+		this.branches.add(branch);
+	}
+
+	HeadOffice getHeadOffice() {
+		return headOffice;
+	}
+
+	void setHeadOffice(HeadOffice headOffice) {
+		this.headOffice = headOffice;
+	}
+	
+}
